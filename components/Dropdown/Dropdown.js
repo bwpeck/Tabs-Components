e5a4fe1c74ba3d60e89248c2fe410a6d0ef993e3
bwpeck <48737515+bwpeck@users.noreply.github.com>
2019-04-18 19:27:58 -0400

    drop done

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,27 +1,27 @@
 class Dropdown {
   constructor(element) {
+    // Assign this.element to the dropdown element
     this.element = element;
-    this.button = this.element.querySelector('.dropdown-button');
-    this.content = this.element.querySelector('.dropdown-content');
+
+    // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+    this.button = this.element.querySelector(".dropdown-button");
+
+    // assign the reference to the ".dropdown-content" class found in the dropdown element
+    this.content = this.element.querySelector(".dropdown-content");
+
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-      this.dropsD()
-    })
-  }
-  dropsD() {
-    this.content.toggleContent()
-  }
-}
-    class Content {
-  constructor(dropElement) {
-    this.dropElement = dropElement;
+    this.button.addEventListener("click", () => {
+      this.toggleContent();
+    });
   }
 
-toggleContent() {
-  this.dropElement.classList.toggle("change");
+  toggleContent() {
+    // Toggle the ".dropdown-hidden" class off and on
+    this.content.classList.toggle("dropdown-hidden");
+  }
 }
-}
-
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document.querySelectorAll('.dropdown').forEach(dropdown => new Dropdown(dropdown));
+let dropdowns = document
+  .querySelectorAll(".dropdown")
+  .forEach(dropdown => new Dropdown(dropdown));
